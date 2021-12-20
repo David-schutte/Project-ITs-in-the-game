6a662da7f2dbfdf2f78a83704cdf03e1c2749a6f
David Schutte <davidschutte02@gmail.com>
2021-12-20 15:22:06 +0100

    added all tiles for 1 path

--- core/src/com/party/board/TileManager.java
+++ core/src/com/party/board/TileManager.java
@@ -17,15 +17,91 @@ public class TileManager {
     }
 
     public void load() {
+        //beginpunt
         addTile(new Tile(63,264));
         addTile(new Tile(63,231));
+        //nieuwe ruimte
         addTile(new Tile(63,200));
+        //splitsing
         addTile(new Tile(38,183));
         addTile(new Tile(24,167));
         addTile(new Tile(24,137));
         addTile(new Tile(24,105));
         addTile(new Tile(24,72));
         addTile(new Tile(24,40));
+        addTile(new Tile(50,40));
+        addTile(new Tile(75,40));
+        addTile(new Tile(101,40));
+        addTile(new Tile(126,40));
+        addTile(new Tile(152,40));
+        addTile(new Tile(178,40));
+        addTile(new Tile(203,40));
+        addTile(new Tile(203,72));
+        //einde splitsing
+        addTile(new Tile(229,72));
+        //nieuwe ruimte
+        addTile(new Tile(254,72));
+        addTile(new Tile(280,72));
+        addTile(new Tile(305,72));
+        //splitsing
+        addTile(new Tile(331,72));
+        addTile(new Tile(357,72));
+        addTile(new Tile(383,72));
+        addTile(new Tile(408,72));
+        addTile(new Tile(434,72));
+        addTile(new Tile(459,72));
+        addTile(new Tile(485,72));
+        addTile(new Tile(510,72));
+        addTile(new Tile(510,104));
+        addTile(new Tile(510,136));
+        //einde splitsing
+        addTile(new Tile(523,167));
+        //nieuwe ruimte
+        addTile(new Tile(549,167));
+        addTile(new Tile(575, 167));
+        addTile(new Tile(588, 183));
+        addTile(new Tile(588, 216));
+        addTile(new Tile(588, 249));
+        addTile(new Tile(588, 280));
+        addTile(new Tile(562, 280));
+        //nieuwe ruimte
+        addTile(new Tile(536, 280));
+        addTile(new Tile(523, 296));
+        addTile(new Tile(523, 328));
+        addTile(new Tile(523, 360));
+        addTile(new Tile(510, 376));
+        addTile(new Tile(484, 376));
+        addTile(new Tile(459, 376));
+        addTile(new Tile(446, 360));
+        addTile(new Tile(446, 328));
+        addTile(new Tile(446, 296));
+        addTile(new Tile(421, 263));
+        addTile(new Tile(395, 263));
+        addTile(new Tile(370, 263));
+        addTile(new Tile(344, 263));
+        addTile(new Tile(319, 296));
+        addTile(new Tile(319, 296));
+        addTile(new Tile(319, 328));
+        addTile(new Tile(319, 360));
+        addTile(new Tile(319, 392));
+        addTile(new Tile(293, 392));
+        addTile(new Tile(267, 392));
+        addTile(new Tile(242, 392));
+        //nieuwe ruimte
+        addTile(new Tile(216, 392));
+        addTile(new Tile(216, 423));
+        addTile(new Tile(190, 423));
+        addTile(new Tile(165, 423));
+        addTile(new Tile(139, 423));
+        addTile(new Tile(114, 423));
+        addTile(new Tile(88, 423));
+        addTile(new Tile(63, 423));
+        addTile(new Tile(63, 392));
+        addTile(new Tile(63, 359));
+        addTile(new Tile(63, 327));
+        addTile(new Tile(63, 296));
+        //einde
+
     }
 
     private void addTile(Tile tile){
