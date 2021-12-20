b8dee74bc6d14a1b0e48cfb8799a54c1a8b395aa
David Schutte <davidschutte02@gmail.com>
2021-12-20 18:19:50 +0100

    added other paths

--- core/src/com/party/board/TileManager.java
+++ core/src/com/party/board/TileManager.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TileManager {
-    Map<Integer, Tile> tileMap = new HashMap<Integer, Tile>();
+    Map<Integer, Tile> tileMap = new HashMap<>();
     public TileManager() {
         //autoLoad();
         load();
@@ -22,7 +22,7 @@ public class TileManager {
         addTile(new Tile(63,231));
         //nieuwe ruimte
         addTile(new Tile(63,200));
-        //splitsing
+//        splitsing onderlangs
         addTile(new Tile(38,183));
         addTile(new Tile(24,167));
         addTile(new Tile(24,137));
@@ -37,13 +37,24 @@ public class TileManager {
         addTile(new Tile(178,40));
         addTile(new Tile(203,40));
         addTile(new Tile(203,72));
+        //splitsing bovenlangs
+        addTile(103, new Tile(88,183));
+        addTile(104, new Tile(113,183));
+        addTile(105, new Tile(138,183));
+        addTile(106, new Tile(164,183));
+        addTile(107, new Tile(189,183));
+        addTile(108, new Tile(215,183));
+        addTile(109, new Tile(215,152));
+        addTile(110, new Tile(215,120));
+        addTile(111, new Tile(215,87));
+
         //einde splitsing
         addTile(new Tile(229,72));
         //nieuwe ruimte
         addTile(new Tile(254,72));
         addTile(new Tile(280,72));
         addTile(new Tile(305,72));
-        //splitsing
+        //splitsing onderlangs
         addTile(new Tile(331,72));
         addTile(new Tile(357,72));
         addTile(new Tile(383,72));
@@ -54,6 +65,17 @@ public class TileManager {
         addTile(new Tile(510,72));
         addTile(new Tile(510,104));
         addTile(new Tile(510,136));
+        //splitsing bovenlangs
+        addTile(121, new Tile(318,87));
+        addTile(122, new Tile(318,120));
+        addTile(123, new Tile(318,152));
+        addTile(124, new Tile(331,167));
+        addTile(125, new Tile(357,167));
+        addTile(126, new Tile(383,167));
+        addTile(127, new Tile(408,167));
+        addTile(128, new Tile(434,167));
+        addTile(129, new Tile(459,167));
+        addTile(130, new Tile(485,167));
         //einde splitsing
         addTile(new Tile(523,167));
         //nieuwe ruimte
@@ -101,7 +123,6 @@ public class TileManager {
         addTile(new Tile(63, 327));
         addTile(new Tile(63, 296));
         //einde
-
     }
 
     private void addTile(Tile tile){
