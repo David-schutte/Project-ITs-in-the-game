f0485d9467538d7e16dd8e3d3767ffb8b9a7d8f5
David Schutte <davidschutte02@gmail.com>
2021-12-22 15:26:49 +0100

    bleh

--- core/src/com/party/test/TestCycle.java
+++ core/src/com/party/test/TestCycle.java
@@ -2,7 +2,6 @@ package com.party.test;
 
 import com.badlogic.gdx.math.Vector2;
 import com.party.Game;
-import com.party.board.Tile;
 import com.party.entity.Entity;
 
 import java.util.ArrayList;
@@ -23,28 +22,23 @@ public class TestCycle {
     }
 
     public void go() {
-
-
        // if(true) return;
-
-
         for (final Entity entity : Game.i().entities) {
             entity.setEndX( Game.i().getTileManager().getTileMap().get(0).getX());
             entity.setEndY( Game.i().getTileManager().getTileMap().get(0).getY());
-
             for (final Integer integer : Game.i().getTileManager().getTileMap().keySet()) {
                 Timer timer = new Timer();
                 TimerTask timerTask = new TimerTask() {
                     @Override
                     public void run() {
                         System.out.println("moving...");
+
                         entity.setEndX(Game.i().getTileManager().getTileMap().get(integer).getX());
                         entity.setEndY(Game.i().getTileManager().getTileMap().get(integer).getY());
                     }
                 };
                 timer.schedule(timerTask, 3000L + integer * 2000L);
             }
-
         }
     }
 }
