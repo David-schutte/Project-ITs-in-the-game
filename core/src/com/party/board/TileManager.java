c2a93d1fe631926c41079be235265250f1d4249c
David Schutte <davidschutte02@gmail.com>
2021-12-23 15:17:10 +0100

    blob

--- core/src/com/party/board/TileManager.java
+++ core/src/com/party/board/TileManager.java
@@ -38,15 +38,15 @@ public class TileManager {
         addTile(new Tile(203,40));
         addTile(new Tile(203,72));
         //splitsing bovenlangs
-        addTile(103, new Tile(88,183));
-        addTile(104, new Tile(113,183));
-        addTile(105, new Tile(138,183));
-        addTile(106, new Tile(164,183));
-        addTile(107, new Tile(189,183));
-        addTile(108, new Tile(215,183));
-        addTile(109, new Tile(215,152));
-        addTile(110, new Tile(215,120));
-        addTile(111, new Tile(215,87));
+//        addTile(103, new Tile(88,183));
+//        addTile(104, new Tile(113,183));
+//        addTile(105, new Tile(138,183));
+//        addTile(106, new Tile(164,183));
+//        addTile(107, new Tile(189,183));
+//        addTile(108, new Tile(215,183));
+//        addTile(109, new Tile(215,152));
+//        addTile(110, new Tile(215,120));
+//        addTile(111, new Tile(215,87));
 
         //einde splitsing
         addTile(new Tile(229,72));
@@ -66,16 +66,16 @@ public class TileManager {
         addTile(new Tile(510,104));
         addTile(new Tile(510,136));
         //splitsing bovenlangs
-        addTile(121, new Tile(318,87));
-        addTile(122, new Tile(318,120));
-        addTile(123, new Tile(318,152));
-        addTile(124, new Tile(331,167));
-        addTile(125, new Tile(357,167));
-        addTile(126, new Tile(383,167));
-        addTile(127, new Tile(408,167));
-        addTile(128, new Tile(434,167));
-        addTile(129, new Tile(459,167));
-        addTile(130, new Tile(485,167));
+//        addTile(121, new Tile(318,87));
+//        addTile(122, new Tile(318,120));
+//        addTile(123, new Tile(318,152));
+//        addTile(124, new Tile(331,167));
+//        addTile(125, new Tile(357,167));
+//        addTile(126, new Tile(383,167));
+//        addTile(127, new Tile(408,167));
+//        addTile(128, new Tile(434,167));
+//        addTile(129, new Tile(459,167));
+//        addTile(130, new Tile(485,167));
         //einde splitsing
         addTile(new Tile(523,167));
         //nieuwe ruimte
